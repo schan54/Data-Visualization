b4f5d8bfce4d0ba15bf102cdf1c98a147f8ff259
Morgan <morganhtrotter@gmail.com>
2019-03-04 21:48:12 -0800

    Updated Bubble Chart

--- co2/map.js
+++ co2/map.js
@@ -84,13 +84,3 @@ function ready(error, data, population) {
       .attr("class", "names")
       .attr("d", path);
 }
-
-/* Toggle between adding and removing the "responsive" class to menubar when the user clicks on the icon */
-function hamFunction() {
-  var x = document.getElementById("mymenu");
-  if (x.className === "menubar") {
-    x.className += " responsive";
-  } else {
-    x.className = "menubar";
-  }
-}
