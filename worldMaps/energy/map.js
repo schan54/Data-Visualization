9a283344c610cc01642fd3a7d1c97978ee604de5
David Krieger <dakriege@ucsc.edu>
2019-03-05 15:30:29 -0800

    Created color scheme for chloropleth

---
+++ worldMaps/energy/map.js
@@ -0,0 +1,94 @@
+var format = d3.format(",");
+
+// Set tooltips
+var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function(d) {
+              return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>" + userYear + ": </strong><span class='details'>" + format(d.value) +"</span>";
+            })
+
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+            width = 1400 - margin.left - margin.right,
+            height = 1000 - margin.top - margin.bottom;
+
+var color = d3.scaleThreshold()
+    .domain([-1000,-500,-100,-25,-5,5,25,100,500,1000])
+    .range(["rgb(105,0,0)", "rgb(254,50,0)", "rgb(254,100,0)", "rgb(254,230,0)", "rgb(254,254,150)", "rgb(200,230,160)","rgb(125,230,125)","rgb(100,230,100)","rgb(50,230,50)","rgb(0,254,0)"]);
+
+var path = d3.geoPath();
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append('g')
+            .attr('class', 'map');
+
+var projection = d3.geoMercator()
+                   .scale(200)
+                  .translate( [width / 2, height / 1.5]);
+
+var path = d3.geoPath().projection(projection);
+
+svg.call(tip);
+
+queue()
+    .defer(d3.json, "world_countries.json")
+    .defer(d3.tsv, "energyChloro.tsv")
+    .await(ready);
+
+var userYear = "2000"
+
+function ready(error, data, population) {
+  var populationById = {};
+
+  population.forEach(function(d) { populationById[d.id] = +d[userYear]; });
+  data.features.forEach(function(d) { d.value = populationById[d.id] });
+
+  svg.append("g")
+      .attr("class", "countries")
+    .selectAll("path")
+      .data(data.features)
+    .enter().append("path")
+      .attr("d", path)
+      .style("fill", function(d) { return color(populationById[d.id]); })
+      .style('stroke', 'white')
+      .style('stroke-width', 1.5)
+      .style("opacity",0.8)
+      // tooltips
+        .style("stroke","white")
+        .style('stroke-width', 0.3)
+        .on('mouseover',function(d){
+          tip.show(d);
+
+          d3.select(this)
+            .style("opacity", 1)
+            .style("stroke","white")
+            .style("stroke-width",3);
+        })
+        .on('mouseout', function(d){
+          tip.hide(d);
+
+          d3.select(this)
+            .style("opacity", 0.8)
+            .style("stroke","white")
+            .style("stroke-width",0.3);
+        });
+
+  svg.append("path")
+      .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
+       // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
+      .attr("class", "names")
+      .attr("d", path);
+}
+
+/* Toggle between adding and removing the "responsive" class to menubar when the user clicks on the icon */
+function hamFunction() {
+  var x = document.getElementById("mymenu");
+  if (x.className === "menubar") {
+    x.className += " responsive";
+  } else {
+    x.className = "menubar";
+  }
+}
