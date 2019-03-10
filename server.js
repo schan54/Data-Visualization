a78614fae23b5016a6c5ca4ab841938be0daac69
Steve Chan <schan54@ucsc.edu>
2019-03-09 16:30:40 -0800

    src

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.static(__dirname));
 app.get("/", function(req, res){
     res.render("index");
 })
-
+app.get('/', function(req, res){
+    res.redirect('/todo');
+ });
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
