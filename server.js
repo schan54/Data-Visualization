ba1a626944a30449f6bea3a118f70564fbba337a
Steve Chan <schan54@ucsc.edu>
2019-03-10 18:55:01 -0700

    srs

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname));
 app.engine('html', require('ejs').renderFile);
 
 app.get("/", function(req, res){
-    res.render("/co2/index");
+    res.render("/co2/index.html");
 })
 
 app.listen(process.env.PORT || 3000, function(){
