b3a75b5147518909c931f666f901e81d8fcb1717
Morgan <morganhtrotter@gmail.com>
2019-04-29 14:32:47 -0700

    Trying to fix button layout

--- src/co2/co2Bubble.js
+++ src/co2/co2Bubble.js
@@ -165,15 +165,6 @@ function select(yearValue) {
         return +d[columnForRadius];
       })]).range([2, 90])
 
-      /*
-      // Simulate forces acting on each node
-      var simulation = d3.forceSimulation(data.filter(function(d) { return d.year == yearTemp}))
-        .force("charge", d3.forceManyBody().strength([-35]))
-        .force("x", d3.forceX())
-        .force("y", d3.forceY())
-        .on("tick", ticked);
-      */
-
         // Simulate forces acting on each node
       var simulation = d3.forceSimulation(data.filter(function(d) { return d.year == yearTemp}))
         .force("charge", d3.forceManyBody().strength(5))
@@ -197,22 +188,6 @@ function select(yearValue) {
           return scaleRadius(d.value)
         }))
         .on('tick', ticked);
-
-      /*
-      function ticked(e) {
-        node.attr("cx", function(d) {
-          return d.x;
-        })
-        .attr("cy", function(d) {
-          return d.y;
-        });
-        node2.attr("cx", function(d) {
-          return d.x;
-        })
-        .attr("cy", function(d) {
-          return d.y;
-        });
-      } */
 
       function ticked() {
         var u = d3.select('svg')
@@ -293,74 +268,7 @@ function select(yearValue) {
 
         u.exit().remove();
       }
-      /*
-      // Remove all elements in chart area before entering
-      d3.selectAll("svg > *").remove();
-
-      // Enter() data
-      var node = svg.selectAll("circle")
-        .data(data.filter(function(d) { return d.year == yearTemp}))
-        .enter()
-        .append("circle")
-        .attr('r', function(d) {
-            return scaleRadius(d[columnForRadius])
-        })
-        .style("fill", function(d) {
-            return colorCircles(d[columnForColors])
-        })
-        .attr('transform', 'translate(' + [width / 2, height / 2] + ')')
-        .on("mouseover", function(d) {
-          if (moving == false) {
-            tooltip.html("<strong>Country: </strong>" + "<span class=\"details\">"
-                + d.country + "</span><br>"
-                + "<strong>" + currentValue + ": </strong>" + "<span class=\"details\">"
-                + d[columnForRadius] + " MtCO2</span>");
-            return tooltip.style("visibility", "visible");
-          }
-        })
-        .on("mousemove", function() {
-          if (moving == false) {
-            return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
-          }
-        })
-        .on("mouseout", function() {
-          if (moving == false) {
-            return tooltip.style("visibility", "hidden");
-          }
-        });
-
-        // Enter() data
-      var node2 = svgNA.selectAll("circle")
-        .data(data.filter(function(d) { return d.year == yearTemp && d.continent == "North America"}))
-        .enter()
-        .append("circle")
-        .attr('r', function(d) {
-            return scaleRadius(d[columnForRadius])
-        })
-        .style("fill", function(d) {
-            return colorCircles(d[columnForColors])
-        })
-        .attr('transform', 'translate(' + [400 / 2, 350 / 2] + ')')
-        .on("mouseover", function(d) {
-          if (moving == false) {
-            tooltip.html("<strong>Country: </strong>" + "<span class=\"details\">"
-                + d.country + "</span><br>"
-                + "<strong>" + currentValue + ": </strong>" + "<span class=\"details\">"
-                + d[columnForRadius] + " MtCO2</span>");
-            return tooltip.style("visibility", "visible");
-          }
-        })
-        .on("mousemove", function() {
-          if (moving == false) {
-            return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
-          }
-        })
-        .on("mouseout", function() {
-          if (moving == false) {
-            return tooltip.style("visibility", "hidden");
-          }
-        });
-      */
+      
       var tempArray = [];
       var tempStringArray = [];
       var NAArray = [];
@@ -399,8 +307,6 @@ function select(yearValue) {
         }
         index++;
       })
-
-
 
       d3.select("#mainChart").selectAll("text").remove();
 
