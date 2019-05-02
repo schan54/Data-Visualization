1e7453758d973002f40cf1028de42d48907fbafd
David Krieger <dakriege@ucsc.edu>
2019-05-02 11:11:33 -0700

    Choropleth Headers complete

--- src/co2/co2Chloro.js
+++ src/co2/co2Chloro.js
@@ -28,10 +28,8 @@ var projection = d3.geoMercator()
                    .scale(200)
                   .translate( [width / 2, height / 1.5 + 150]);
 
-  svg.append("line").attr("x1", 0).attr("y1", 200).attr("x2", 1500).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
-  svg.append("line").attr("x1", 370).attr("y1", 0).attr("x2", 370).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
-  svg.append("line").attr("x1", 580).attr("y1", 0).attr("x2", 580).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
-  svg.append("line").attr("x1", 820).attr("y1", 0).attr("x2", 820).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
+
+
 
 var path = d3.geoPath().projection(projection);
 
@@ -44,16 +42,127 @@ queue()
 
 var userYear = formatDateIntoYear(startDate);
 
+svg.append("line").attr("x1", 0).attr("y1", 200).attr("x2", 1500).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
+svg.append("line").attr("x1", 370).attr("y1", 0).attr("x2", 370).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
+svg.append("line").attr("x1", 580).attr("y1", 0).attr("x2", 580).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
+svg.append("line").attr("x1", 820).attr("y1", 0).attr("x2", 820).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
+var compareString = "1960 vs 1960";
+svg.append("text").html(compareString).attr("x", 390).attr("y", 120).attr("id", "choroYear");
+
 function ready(error, data, population) {
+  d3.select("#choroYear").remove()
   var populationById = {};
 
   if(document.getElementById('Compare').checked) {
     population.forEach(function(d) { populationById[d.id] = +d[userYear] - +d[1960]; });
+    compareString = userYear + " vs " + 1960;
+    svg.append("text").html(compareString).attr("x", 390).attr("y", 120).attr("id", "choroYear");
   }
 
   else if(document.getElementById('Isolated').checked) {
     population.forEach(function(d) { populationById[d.id] = +d[userYear]; });
+    svg.append("text").html(userYear).attr("x", 390).attr("y", 120).attr("id", "choroYear");
   }
+
+  var allCountries = [];
+
+  function bubbleSort(a){
+    var swapp;
+    var n = a.length-1;
+
+    var x=a;
+    do {
+        swapp = false;
+        for (var i=0; i < n; i++)
+        {
+
+            if (Number(x[i][1]) > Number(x[i+1][1])) {
+               var temp = x[i];
+               x[i] = x[i+1];
+               x[i+1] = temp;
+               swapp = true;
+            }
+        }
+        n--;
+    } while (swapp);
+  allCountries = x;
+  return;
+  }
+
+
+
+  function biggest5(hashmap) {
+
+    tea = Object.values(hashmap)
+
+    //console.log(Object.values(tea[1]))
+
+    var yearIndex = userYear - 1960
+    for (var index in tea) {
+      //console.log(Object.keys(tea[index]));
+      var countryKeys = (Object.keys(tea[index]));
+      var countryValues = (Object.values(tea[index]));
+      var countryArray = [];
+
+      countryArray.push(countryKeys[yearIndex]);
+      countryArray.push(countryValues[yearIndex]);
+      countryArray.push(countryValues[countryKeys.length-1]);
+      if (isNaN(countryArray[1]) || countryArray[1] == "" || !(isNaN(countryArray[2]))) {
+      }
+      else {
+        allCountries.push(countryArray);
+      }
+    }
+    //console.log(allCountries);
+    bubbleSort(allCountries);
+    return;
+  }
+  biggest5(population);
+
+  max5 = allCountries.slice((allCountries.length - 5), allCountries.length);
+  min5 = allCountries.slice(0, 5);
+
+  function displayMin(a) {
+    d3.select("#top1").remove();
+    d3.select("#top2").remove();
+    d3.select("#top3").remove();
+    d3.select("#top4").remove();
+    d3.select("#top5").remove();
+
+    svg.append("text").html("1. " + a[4][2] + ": " + a[4][1] + " MtCO2").attr("id", "top1").attr("x", 850).attr("y", 80);
+    svg.append("text").html("2. " + a[3][2] + ": " + a[3][1] + " MtCO2").attr("id", "top2").attr("x", 850).attr("y", 100);
+    svg.append("text").html("3. " + a[2][2] + ": " + a[2][1] + " MtCO2").attr("id", "top3").attr("x", 850).attr("y", 120);
+    svg.append("text").html("4. " + a[1][2] + ": " + a[1][1] + " MtCO2").attr("id", "top4").attr("x", 850).attr("y", 140);
+    svg.append("text").html("5. " + a[0][2] + ": " + a[0][1] + " MtCO2").attr("id", "top5").attr("x", 850).attr("y", 160);
+  }
+
+  function displayMax(a) {
+    d3.select("#top1").remove();
+    d3.select("#top2").remove();
+    d3.select("#top3").remove();
+    d3.select("#top4").remove();
+    d3.select("#top5").remove();
+
+    svg.append("text").html("1. " + a[0][2] + ": " + a[0][1] + " MtCO2").attr("id", "top1").attr("x", 850).attr("y", 80);
+    svg.append("text").html("2. " + a[1][2] + ": " + a[1][1] + " MtCO2").attr("id", "top2").attr("x", 850).attr("y", 100);
+    svg.append("text").html("3. " + a[2][2] + ": " + a[2][1] + " MtCO2").attr("id", "top3").attr("x", 850).attr("y", 120);
+    svg.append("text").html("4. " + a[3][2] + ": " + a[3][1] + " MtCO2").attr("id", "top4").attr("x", 850).attr("y", 140);
+    svg.append("text").html("5. " + a[4][2] + ": " + a[4][1] + " MtCO2").attr("id", "top5").attr("x", 850).attr("y", 160);
+  }
+
+  function displayTotal() {
+    d3.select("#totalSum").remove();
+    var totalSum = 0;
+    for (index in allCountries) {
+      totalSum = totalSum + Number(allCountries[index][1]);
+    }
+    svg.append("text").html("World's Total Emissions: " + Math.floor(totalSum)).attr("id", "totalSum").attr("x", 600).attr("y", 70);
+  }
+
+  displayTotal();
+  displayMax(max5);
+
+
 
   data.features.forEach(function(d) { d.value = populationById[d.id] });
 
