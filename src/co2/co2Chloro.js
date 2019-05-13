e544cf69f8a2272b735a8e3e05ef0756111000c3
David Krieger <dakriege@ucsc.edu>
2019-05-13 16:30:25 -0700

    Fixed CSS of co2Choro

--- src/co2/co2Chloro.js
+++ src/co2/co2Chloro.js
@@ -23,44 +23,51 @@ svg.append("line").attr("x1", 370).attr("y1", 0).attr("x2", 370).attr("y2", 200)
 svg.append("line").attr("x1", 580).attr("y1", 0).attr("x2", 580).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
 svg.append("line").attr("x1", 820).attr("y1", 0).attr("x2", 820).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
 
-var compareString = "1960 vs 1960";
-svg.append("text").html(compareString).attr("x", 390).attr("y", 120).attr("id", "choroYear");
-
 function ready(error, data, population) {
 
-  d3.select("#choroYear").remove()
+  d3.select("#choroYear1").remove()
+  d3.select("#choroYear2").remove()
+  d3.select("#vs").remove()
   var populationById = {};
   var max5, min5;
 
   if(compareActive == true) {
     population.forEach(function(d) { populationById[d.id] = +d[userYear] - +d[userYear2]; });
-    compareString = userYear + " vs " + 1960;
-    svg.append("text").html(compareString).attr("x", 390).attr("y", 120).attr("id", "choroYear");
+    svg.append("text").html(userYear).attr("x", 410).attr("y", 50).attr("id", "choroYear1");
+    svg.append("text").html("vs").attr("x", 455).attr("y", 85).attr("id", "vs");
+    svg.append("text").html(userYear2).attr("x", 410).attr("y", 145).attr("id", "choroYear2");
 
     var sortedHash1 = sortHash(population, userYear);
     var sortedHash2 = sortHash(population, userYear2);
 
+    setupCompareSlider();
 
-
-    displayTotalCompare(sortedHash1, sortedHash2);
+    displaySumCompare(sortedHash1, sortedHash2);
     displayMaxCompare(sortedHash1, sortedHash2);
 
+    //displayPercentCompare(sortedHash1, sortedHash2);
+
+    tip.html(function(d) {
+      return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>" + userYear + " vs " + userYear2 +": </strong><span class='details'>" + format(d.value) +"</span>";
+    })
   }
 
   else {
     population.forEach(function(d) { populationById[d.id] = +d[userYear]; });
-    svg.append("text").html(userYear).attr("x", 390).attr("y", 120).attr("id", "choroYear");
+    svg.append("text").html(userYear).attr("x", 390).attr("y", 120).attr("id", "choroYear1");
 
     var sortedHash = sortHash(population, userYear);
 
     max5 = sortedHash.slice((sortedHash.length - 5), sortedHash.length);
     min5 = sortedHash.slice(0, 5);
 
-    displayTotalIso(sortedHash);
-    displayMaxIso(max5);
+    displaySumIso(sortedHash);
+    displayMaxIso(max5, min5);
+
+    tip.html(function(d) {
+      return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>" + userYear + ": </strong><span class='details'>" + format(d.value) +"</span>";
+    })
   }
-
-
 
   data.features.forEach(function(d) { d.value = populationById[d.id] });
 
