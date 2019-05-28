cf74a33246c13b3f233cdf4f8fce84ae48e8414a
David Krieger <dakriege@ucsc.edu>
2019-05-28 06:09:12 -0700

    Home page is just mission statement

--- src/home/home.js
+++ src/home/home.js
@@ -1,5 +1,32 @@
 var textBox = d3.select("body").append("svg")
       .attr('width', 1100)
+      .attr('height', 250)
+      .attr('id', "textSVG");
+
+	textBox.append("text").html("VISUALIZING CLIMATE CHANGE")
+      .attr('x', 50)
+      .attr('y', 50)
+      .style('font-family', "Montserrat, sans-serif")
+      .style('font-size', 55)
+      .style('letter-spacing', 2);
+	  
+	textBox.append("text").html("Mission Statement: Our goal is to provide users with unique and meaningful visualizations for understanding climate")
+      .attr('x', 100)
+      .attr('y', 150)
+      .attr('class', 'mission');
+
+    textBox.append("text").html("change. We believe that while one visual may provide great insight into CO2 emissions, the same visual might not be")
+      .attr('x', 100)
+      .attr('y', 180)
+      .attr('class', 'mission');
+
+    textBox.append("text").html("applicable or relevant for another dataset such as temperature.")
+      .attr('x', 100)
+      .attr('y', 210)
+      .attr('class', 'mission');
+	  
+/*var textBox = d3.select("body").append("svg")
+      .attr('width', 1100)
       .attr('height', 2180)
       .attr('id', "textSVG");
 
@@ -180,4 +207,4 @@ var textBox = d3.select("body").append("svg")
 
     textBox.append("text").html("example, public transport upgrades, energy efficiency improvements, sustainable city planning, etc.). It’s up to us what happens next.")
       .attr('x', 100)
-      .attr('y', 2140);
+      .attr('y', 2140);*/
