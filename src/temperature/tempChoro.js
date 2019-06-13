e868f4c1ca96ec7c555e16399a9a715eb34dbb09
David Krieger <dakriege@ucsc.edu>
2019-06-12 18:15:10 -0700

    Added more comments to choropleth files

--- src/temperature/tempChoro.js
+++ src/temperature/tempChoro.js
@@ -1,4 +1,4 @@
-// Set tooltips
+//Create color scale based on compare or isolated
 var colorCompare = d3.scaleThreshold()
     .domain(colorCompareDomain)
     .range([d3.interpolateRdYlBu(1), d3.interpolateRdYlBu(0.9), d3.interpolateRdYlBu(0.8), d3.interpolateRdYlBu(0.7),
@@ -11,77 +11,102 @@ var colorIsolated = d3.scaleThreshold()
           d3.interpolateRdYlBu(0.6), d3.interpolateRdYlBu(0.5), d3.interpolateRdYlBu(0.4), d3.interpolateRdYlBu(0.3),
 					d3.interpolateRdYlBu(0.2), d3.interpolateRdYlBu(0.1), d3.interpolateRdYlBu(0.0)]);
 
+//Draws the world map
 var projection = d3.geoMercator()
                    .scale(200)
                   .translate( [width / 2, height / 1.5 + 150]);
 
 var path = d3.geoPath().projection(projection);
 
+//Apply tooltips to the map
 choroSvg.call(tip);
 
+//First render of the choropleth with proper data
 queue()
     .defer(d3.json, "../core/world_countries.json")
     .defer(d3.tsv, "worldTemp.tsv")
     .await(ready);
 
+//Draw the header boxes
 choroSvg.append("line").attr("x1", 370).attr("y1", 0).attr("x2", 370).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
 choroSvg.append("line").attr("x1", 580).attr("y1", 0).attr("x2", 580).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
 choroSvg.append("line").attr("x1", 820).attr("y1", 0).attr("x2", 820).attr("y2", 200).attr("stroke-width", 0.5).attr("stroke", "black");
 
+
+//Main function for choropleth
 function ready(error, data, population) {
 
+	//Undraw the Year box
   d3.select("#choroYear1").remove()
   d3.select("#choroYear2").remove()
   d3.select("#vs").remove()
+
+	//Definte some vars
   var populationById = {};
   var max5, min5;
 
+	//If compare selected do this
   if(compareActive == true) {
 
+		//Load the data
     population.forEach(function(d) { populationById[d.id] = +d[userYear] - +d[userYear2]; });
+
+		//Draw the Year section of header
 		choroSvg.append("text").html(userYear2).attr("x", 410).attr("y", 65).attr("id", "choroYear2");
     choroSvg.append("text").html("vs").attr("x", 455).attr("y", 100).attr("id", "vs");
     choroSvg.append("text").html(userYear).attr("x", 410).attr("y", 160).attr("id", "choroYear1");
 
+		//Sort the hash of each user yaer based on size
     var sortedHash1 = sortHash(population, userYear);
     var sortedHash2 = sortHash(population, userYear2);
 
+		//Functions to create the sum box and top5 box
     displaySumCompare(sortedHash1, sortedHash2);
     displayMaxCompare(sortedHash1, sortedHash2);
 
     //displayPercentCompare(sortedHash1, sortedHash2);
 
+		//apply tooltip to html interaction
     tip.html(function(d) {
       return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>" + userYear + " vs " + userYear2 +": </strong><span class='details'>" + format(d.value) +"</span>";
     })
   }
 
+	//Isolate filter selected
   else {
 
+		//Load data
     population.forEach(function(d) { populationById[d.id] = +d[userYear]; });
-    choroSvg.append("text").html(userYear).attr("x", 415).attr("y", 120).attr("id", "choroYear1");
 
+		//Draw the Year box
+		choroSvg.append("text").html(userYear).attr("x", 415).attr("y", 120).attr("id", "choroYear1");
+
+		//Sort the data
     var sortedHash = sortHash(population, userYear);
 
+		//Grab the top and bottom 5 of the isolated year
     max5 = sortedHash.slice((sortedHash.length - 5), sortedHash.length);
     min5 = sortedHash.slice(0, 5);
 
     displaySumIso(sortedHash);
     displayMaxIso(max5, min5);
 
-
+		//Apply tooltip to HTML
     tip.html(function(d) {
       return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>" + userYear + ": </strong><span class='details'>" + format(d.value) +"</span>";
     })
   }
 
+	//Map the data loaded to the country tags on the map
   data.features.forEach(function(d) { d.value = populationById[d.id] });
 
 	d3.selectAll("path").remove();
 
+	//Time to draw the colors!
   choroSvg.append("g")
     .attr("class", "countries")
-    .selectAll("path")
+
+		.selectAll("path")
 			.remove()
       .data(data.features)
     .enter().append("path")
--- src/temperature/tempChoroButtons.js
+++ src/temperature/tempChoroButtons.js
@@ -61,6 +61,7 @@ var buttonGroups= choroButtonContainer.selectAll("g.button")
           .await(ready);
       }
   })
+	//Mouse elements, hover and out
   .on("mouseover", function() {
       if (d3.select(this).select("rect").attr("fill") != pressedColorChoro) {
           d3.select(this)
@@ -76,6 +77,7 @@ var buttonGroups= choroButtonContainer.selectAll("g.button")
       }
   })
 
+//Define the button sizes
 var bWidthChoro= 100; //button width
 var bHeightChoro= 25; //button height
 var bSpaceChoro= 10; //space between buttons
@@ -94,6 +96,7 @@ buttonGroups.append("rect")
 		.attr("ry",5)
 		.attr("fill",defaultColorChoro)
 
+//Add text to the button locations
 buttonGroups.append("text")
 		.attr("class","choroButtonText")
 		.attr("x",function(d,i) {return x0Choro+(bWidthChoro+bSpaceChoro)*(i%2)  + bWidthChoro/2;})
@@ -103,7 +106,7 @@ buttonGroups.append("text")
 		.attr("fill","white")
 		.text(function(d) {return d;})
 
-
+//Create text input box
 var foreign1 = choroSvg.append("svg:foreignObject")
 	.attr("width", bWidthChoro* 2.5 + bSpaceChoro)
 	.attr("height", bHeightChoro+10)
@@ -115,20 +118,25 @@ var foreign1 = choroSvg.append("svg:foreignObject")
 	.style("resize", 'none')
 	.append("xhtml:body")
 
+	//
 	.html("<input oninput='return inputRecv()' id='inputbox' class='foo' rows='13' cols='40' style='text-align:center; color: white;' type='text' placeholder='Input Date Or Use Slider'> </input>")
 //	.html("<input oninput='return inputRecv()' style='font: Times; resize: none; font-size: 15pt; border 1px solid lightgray; outline: none; border-radius: 10px;' id='inputbox' class='foo' rows='13' cols='40' type='text'> </input>")
 
+
+//Actively checks the input box
 function inputRecv() {
 	var inputString = document.getElementById("inputbox").value;
 	inputString = inputString.replace(/ /g,'')
 	var splitString = inputString.split(',')
+
+	//If compare button selected, search for 2 strings of numbers with the right number
 	if (compareActive) {
 		if (((1901 <= parseInt(splitString[0])) && (parseInt(splitString[0])<= 2014))
 		&& ((1901 <= parseInt(splitString[1])) && (parseInt(splitString[1])<= 2014))) {
 			userYear = splitString[1]
 			userYear2 = splitString[0]
 
-
+			//Update the slider locations
 			handle.attr("cx", x(new Date(userYear)));
 			label
 				.attr("x", x(new Date(userYear)))
@@ -138,22 +146,29 @@ function inputRecv() {
 			label2
 				.attr("x", x(new Date(userYear2)))
 				.text(userYear2);
+
+			//Reload the world map
 			queue()
 				.defer(d3.json, "../core/world_countries.json")
 				.defer(d3.tsv, "worldTemp.tsv")
 				.await(ready);
 		}
 	}
+
+	//If isolated button selected, search for only 1 string of numbers
 	else {
 		if ((parseInt(splitString[0]) >= 1901)
 		&& (parseInt(splitString[0]) <= 2014)
 		&& (parseInt(splitString[0]) != userYear)) {
 			userYear = parseInt(splitString[0]);
+
+			//update the slider locations
 			handle.attr("cx", x(new Date(userYear)));
 			label
 				.attr("x", x(new Date(userYear)))
 				.text(userYear);
 
+			//Reload the world map
 			queue()
 				.defer(d3.json, "../core/world_countries.json")
 				.defer(d3.tsv, "worldTemp.tsv")
@@ -162,7 +177,7 @@ function inputRecv() {
 	}
 }
 
-
+//Manages the color changing of the buttons
 function updateChoroButtonColors(button, parent) {
     parent.selectAll("rect")
             .attr("fill",defaultColorChoro)
