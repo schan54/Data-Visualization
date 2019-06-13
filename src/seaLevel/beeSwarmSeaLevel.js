40ba55f992afa82d1454722716e6e52aac1f4ed0
Alexandra Tutino <alexandratutino@gmail.com>
2019-06-13 06:19:18 -0700

    Add files via upload

--- src/seaLevel/beeSwarmSeaLevel.js
+++ src/seaLevel/beeSwarmSeaLevel.js
@@ -1,17 +1,17 @@
-
+// visual js for bee swarm
 
 var width = 1360,
 	height = 550,
 	radius = 10;
 	
 
-var svgBee = d3.select('body').append('#svgBee')
+var svg = d3.select('body').append('svg')
 			.attr('width', 1300)
 			.attr('height', 600)
 			.attr("id", "graph-2")
 
-
-svgBee.append("line", '#svgBee')
+// middle line
+svg.append("line", 'svg')
 	.classed('main_line', true)
 	.attr("x1", 0)
 	.attr("y1", height/2)
@@ -19,6 +19,141 @@ svgBee.append("line", '#svgBee')
 	.attr("y2", height/2)
 	.attr("stroke-width", 1.5)
 	.attr("stroke", "#A3A0A6");
+
+	//text for 4.375 mm
+	var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '325/2');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '2';
+	svg.append('text')
+
+// line for  145 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 650) //650 = 1300/2
+	.attr("y1", 0)
+	.attr("x2", 650) //650 =1300/2
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 145 mm
+	var text7 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '650');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '145 mm';
+	svg.append('text7')
+
+// line for 51.25 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 325) //325 = 1300/4
+	.attr("y1", 0)
+	.attr("x2", 325) //325 = 1300/4
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 51.25 mm
+	var text6 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '325');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '51.25 mm';
+	svg.append('text6')
+
+// line for 238.75 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 975) // 325+650
+	.attr("y1", 0)
+	.attr("x2", 975)
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 238.75 mm
+	var text5 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '975');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '238.75 mm';
+	svg.append('text5')
+
+// line for 4.375 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 325/2) //325 = 1300/4
+	.attr("y1", 0)
+	.attr("x2", 325/2) //325 = 1300/4
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 4.375 mm
+	var text4 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '325/2');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '4.375 mm';
+	svg.append('text4')
+
+
+// line for 98.125 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 975/2) // 325+650
+	.attr("y1", 0)
+	.attr("x2", 975/2)
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 98.125 mm
+	var text3 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '975/2');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '98.125 mm';
+	svg.append('text3')
+
+	// line for 191.875 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 812.5) // 162.5+650
+	.attr("y1", 0)
+	.attr("x2", 812.5)
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 191.875 mm
+	var text2 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '812.5');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '191.875 mm';
+	svg.append('text2')
+
+	// line for 285.625 mm
+	svg.append("line", 'svg')
+	.classed('middle_line', true)
+	.attr("x1", 1137.5) // 162.5+975
+	.attr("y1", 0)
+	.attr("x2", 1137.5)
+	.attr("y2", height)
+	.attr("stroke-width", 1.5)
+	.attr("stroke", "#A3A0A6");
+
+	//text for 285.625 mm
+	var text1 = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+	text.setAttribute('x', '1137.5');
+	text.setAttribute('y', 'height/2');
+	text.setAttribute('fill', '#000');
+	text.textContent = '285.625 mm';
+	svg.append('text1')
 
 
 var x = d3.scaleLinear()
@@ -46,7 +181,7 @@ d3.json('sealevelbeeswarm2.json', function(data){
 	}
 
 
-	svgBee.selectAll('.circ')
+	svg.selectAll('.circ')
 		.data(data)
 		.enter().append('circle').classed('circ', true)
 		.attr('r', radius)
@@ -79,53 +214,7 @@ d3.json('sealevelbeeswarm2.json', function(data){
 		simulation.alphaDecay(0.1);
 	}, 8000);
 
-	var buttons = d3.select('body').append('div');
-	buttons.append('button').text('1970').attr('value', '1970').classed('d_sel', true)
-	buttons.append('button').text('1971').attr('value', '1971').classed('d_sel', true)
-	buttons.append('button').text('1972').attr('value', '1972').classed('d_sel', true)
-	buttons.append('button').text('1973').attr('value', '1973').classed('d_sel', true)
-	buttons.append('button').text('1974').attr('value', '1974').classed('d_sel', true)
-	buttons.append('button').text('1975').attr('value', '1975').classed('d_sel', true)
-	buttons.append('button').text('1976').attr('value', '1976').classed('d_sel', true)
-	buttons.append('button').text('1977').attr('value', '1977').classed('d_sel', true)
-	buttons.append('button').text('1978').attr('value', '1978').classed('d_sel', true)
-	buttons.append('button').text('1979').attr('value', '1979').classed('d_sel', true)
-	buttons.append('button').text('1980').attr('value', '1980').classed('d_sel', true)
-	buttons.append('button').text('1981').attr('value', '1981').classed('d_sel', true)
-	buttons.append('button').text('1982').attr('value', '1982').classed('d_sel', true)
-	buttons.append('button').text('1983').attr('value', '1983').classed('d_sel', true)
-	buttons.append('button').text('1984').attr('value', '1984').classed('d_sel', true)
-	buttons.append('button').text('1985').attr('value', '1985').classed('d_sel', true)
-	buttons.append('button').text('1986').attr('value', '1986').classed('d_sel', true)
-	buttons.append('button').text('1987').attr('value', '1987').classed('d_sel', true)
-	buttons.append('button').text('1988').attr('value', '1988').classed('d_sel', true)
-	buttons.append('button').text('1989').attr('value', '1989').classed('d_sel', true)
-	buttons.append('button').text('1990').attr('value', '1990').classed('d_sel', true)
-	buttons.append('button').text('1991').attr('value', '1991').classed('d_sel', true)
-	buttons.append('button').text('1992').attr('value', '1992').classed('d_sel', true)
-	buttons.append('button').text('1993').attr('value', '1993').classed('d_sel', true)
-	buttons.append('button').text('1994').attr('value', '1994').classed('d_sel', true)
-	buttons.append('button').text('1995').attr('value', '1995').classed('d_sel', true)
-	buttons.append('button').text('1996').attr('value', '1996').classed('d_sel', true)
-	buttons.append('button').text('1997').attr('value', '1997').classed('d_sel', true)
-	buttons.append('button').text('1998').attr('value', '1998').classed('d_sel', true)
-	buttons.append('button').text('1999').attr('value', '1999').classed('d_sel', true)
-	buttons.append('button').text('2000').attr('value', '2000').classed('d_sel', true)
-	buttons.append('button').text('2001').attr('value', '2001').classed('d_sel', true)
-	buttons.append('button').text('2002').attr('value', '2002').classed('d_sel', true)
-	buttons.append('button').text('2003').attr('value', '2003').classed('d_sel', true)
-	buttons.append('button').text('2004').attr('value', '2004').classed('d_sel', true)
-	buttons.append('button').text('2005').attr('value', '2005').classed('d_sel', true)
-	buttons.append('button').text('2006').attr('value', '2006').classed('d_sel', true)
-	buttons.append('button').text('2007').attr('value', '2007').classed('d_sel', true)
-	buttons.append('button').text('2008').attr('value', '2008').classed('d_sel', true)
-	buttons.append('button').text('2009').attr('value', '2009').classed('d_sel', true)
-	buttons.append('button').text('2010').attr('value', '2010').classed('d_sel', true)
-	buttons.append('button').text('2011').attr('value', '2011').classed('d_sel', true)
-	buttons.append('button').text('2012').attr('value', '2012').classed('d_sel', true)
-	buttons.append('button').text('2013').attr('value', '2013').classed('d_sel', true)
-	buttons.append('button').text('2025').attr('value', '2025').classed('d_sel', true)
-	buttons.append('button').text('2050').attr('value', '2050').classed('d_sel', true)
+	
 	
 	
 
